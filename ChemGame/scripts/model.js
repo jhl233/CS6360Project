386935441860ad374f90d3b7ac5f287a5ac25d37
licn333 <licn333@gmail.com>
2016-10-26 20:38:27 -0400

    Revert "Minus"
    This reverts commit 2a35b06589d0581a960e3b6209782b2618ac59ef.

--- ChemGame/scripts/model.js
+++ ChemGame/scripts/model.js
@@ -24,28 +24,11 @@ var stateModule = (function(viewModule, levelModule) {
             currentState["reactants"][reactant] = currentLevel["reactants"][reactant];
         }
 
-<<<<<<< Updated upstream
 		i = 1;
 		for (var product in currentLevel["products"]) {
             currentState["products"][product] = currentLevel["products"][product];
 	   	}
 	   	currentState["svgmap"] = currentLevel["svgmap"];
-=======
-        $clickable = $("<img>", {class: "shadow pic", src: "svg/" + reactantSVG, "data-name": reactant})
-        $clickable.click(function(event) {
-            addReactant($(event.target).data("name"));
-        });
-        $reactant.append($("<div>", {class: "reactant-badge", text: "0", id: reactant+"ReactantCoeff"}));
-        $reactant.append($clickable);
-        $reactant.append($("<div>", {class: "reactant-minus-button", text: "-"}));
-    }
-    
-    // Adds the clickable products to the view
-    i = 1;
-    for (var product in level["products"]) {
-        var $product = $("<div>", {class: "bottom-box product-box-" + (i++)});
-        currentState["products"][product] = 0;
->>>>>>> Stashed changes
 
 		var callBacks = {
 			addReactant: addReactant,
@@ -57,7 +40,6 @@ var stateModule = (function(viewModule, levelModule) {
 		viewModule.initializeScreen(currentState, callBacks);
    	}
 
-<<<<<<< Updated upstream
 	function addReactant(reactant) {
 	   	currentState["reactants"][reactant] += 1;
 	   	var coeff = $("#" + reactant + "ReactantCoeff");
@@ -72,20 +54,6 @@ var stateModule = (function(viewModule, levelModule) {
 			viewModule.openOverlay();
 		}	
 	}
-=======
-        $clickable = $("<img>", {class: "shadow pic4", src: "svg/" + productSVG, "data-name": product});
-        $clickable.click(function(event) {
-            addProduct($(event.target).data("name"));
-        });
-        $product.append($("<div>", {class: "product-badge", text: "0", id: product+"ProductCoeff"}));
-        $product.append($clickable);
-        $product.append($("<div>", {class: "product-minus-button", text: "-"}));
-    }
-    currentState["svgmap"] = level["svgmap"];
-    console.log("Done!");
-    console.log(currentState);
-}
->>>>>>> Stashed changes
 
 	function addProduct(product) {
 	   	currentState["products"][product] += 1;
