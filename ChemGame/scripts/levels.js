6730652a42ddd490a3131a2c2f53129bd18abfb8
salonijoshi26 <salonijoshi@dhcp-rhodes-1483.eduroam.cornell.edu>
2016-11-03 19:46:44 -0400

    H2O+Na->NaOH+H2 levels implemented

--- ChemGame/scripts/levels.js
+++ ChemGame/scripts/levels.js
@@ -110,6 +110,97 @@ var levelModule = (function() {
                 "Chicken2Plate":   "pink_plate.svg"
             }
         },
+        
+        // Na+H2O->NaOH+H2: v1
+        {
+            "reactants": {
+                "Chicken2Bacon": 0,
+                "Bread": 0
+            },
+            "products": {
+                "BreadBaconChicken": 0,
+                "Chicken2": 0
+            },
+            "svgmap": {
+                "Chicken2Bacon":   "l6/v1/svg-reactant-H2O.svg",
+                "Bread":             "l6/v1/svg-reactant-Na.svg",
+                "BreadBaconChicken":      "l6/v1/svg-product-NaOH.svg",
+                "Chicken2":        "l6/v1/svg-product-H2.svg",
+                "aBread":        "l6/svg-bread.svg",
+                "aBacon":          "svg-bacon.svg",
+                "aChicken":        "svg-chicken.svg",
+                "BreadBaconChickenPlate": "grey_plate.svg",
+                "Chicken2Plate":   "pink_plate.svg"
+            }
+        },
+        
+        // Na+H2O->NaOH+H2: v2
+        {
+            "reactants": {
+                "Chicken2Bacon": 0,
+                "Sodium": 0
+            },
+            "products": {
+                "SodiumBaconChicken": 0,
+                "Chicken2": 0
+            },
+            "svgmap": {
+                "Chicken2Bacon":   "l6/v2/svg-reactant-H2O.svg",
+                "Sodium":             "l6/v2/svg-reactant-Na.svg",
+                "SodiumBaconChicken":      "l6/v2/svg-product-NaOH.svg",
+                "Chicken2":        "l6/v2/svg-product-H2.svg",
+                "aSodium":        "l6/svg-atom-Na.svg",
+                "aBacon":          "svg-bacon.svg",
+                "aChicken":        "svg-chicken.svg",
+                "SodiumBaconChickenPlate": "grey_plate.svg",
+                "Chicken2Plate":   "pink_plate.svg"
+            }
+        },
+        
+         // Na+H2O->NaOH+H2: v3
+        {
+            "reactants": {
+                "Chicken2Oxygen": 0,
+                "Sodium": 0
+            },
+            "products": {
+                "SodiumOxygenChicken": 0,
+                "Chicken2": 0
+            },
+            "svgmap": {
+                "Chicken2Oxygen":   "l6/v3/svg-reactant-H2O.svg",
+                "Sodium":             "l6/v3/svg-reactant-Na.svg",
+                "SodiumOxygenChicken":      "l6/v3/svg-product-NaOH.svg",
+                "Chicken2":        "l6/v3/svg-product-H2.svg",
+                "aSodium":        "l6/svg-atom-Na.svg",
+                "aOxygen":          "svg-o-atom.svg",
+                "aChicken":        "svg-chicken.svg",
+                "SodiumOxygenChickenPlate": "grey_plate.svg",
+                "Chicken2Plate":   "pink_plate.svg"
+            }
+        },
+        
+        {
+            "reactants": {
+                "Hydrogen2Oxygen": 0,
+                "Sodium": 0
+            },
+            "products": {
+                "SodiumOxygenHydrogen": 0,
+                "Hydrogen2": 0
+            },
+            "svgmap": {
+                "Hydrogen2Oxygen":   "l6/v4/svg-reactant-H2O.svg",
+                "Sodium":             "l6/v4/svg-reactant-Na.svg",
+                "SodiumOxygenHydrogen":      "l6/v4/svg-product-NaOH.svg",
+                "Hydrogen2":        "l6/v4/svg-product-H2.svg",
+                "aSodium":        "l6/svg-atom-Na.svg",
+                "aOxygen":          "svg-o-atom.svg",
+                "aHydrogen":        "l6/svg-atom-H.svg",
+                "SodiumOxygenHydrogenPlate": "grey_plate.svg",
+                "Hydrogen2Plate":   "pink_plate.svg"
+            }
+        },
     ];
 	console.log("Levels: ");
 	console.log(module.levels);
--- ChemGame/scripts/load.js
+++ ChemGame/scripts/load.js
@@ -30,7 +30,7 @@ function showLevels() {
 
     var $levelBox = $("<div>", {class: "level_box"});
     
-    var numLevels = 7;
+    var numLevels = 10;
     for (var i = 1; i <= numLevels; i++) {
          $levelBox.append("<div class='pot_box shadow' onClick='stateModule.initializeLevel(" 
                           + i + ")'>Lv." + i + "</div>");
